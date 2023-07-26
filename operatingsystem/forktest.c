a0f7921cdc87e8cebace808f9ee7a151929041d5
Kang Minchul <tegongkang@gmail.com>
2023-07-26 22:40:45 +0900

    Create forktest.c

---
+++ operatingsystem/forktest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    // fork() 시스템 콜을 호출하여 자식 프로세스 생성
+    int pid = fork();
+
+    if (pid == -1) {
+        // fork 실패
+        perror("fork");
+        return 1;
+    } else if (pid == 0) {
+        // 자식 프로세스  
+        printf("자식 프로세스입니다.\n");
+    } else {
+        // 부모 프로세스  
+        printf("부모 프로세스입니다. 자식 프로세스 ID: %d\n", pid);
+    }
+
+    return 0;
+}
