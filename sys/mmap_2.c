e3a25776eb8370295ea2ca9c320b79b6f9d30b8a
Kang Minchul <tegongkang@gmail.com>
2023-08-13 03:15:54 +0900

    Update mmap_2.c

--- sys/mmap_2.c
+++ sys/mmap_2.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 
 int main() {
-    size_t size = getpagesize();  // 페이지 크기 가져오기
+    size_t size = getpagesize();
 
     int *shared_data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     if (shared_data == MAP_FAILED) {
@@ -14,7 +14,7 @@ int main() {
         return 1;
     }
 
-    *shared_data = 42;  // 공유 데이터에 값 설정
+    *shared_data = 42;  // 공유 데이터에 값을 설정
 
     pid_t pid = fork();
     if (pid < 0) {
@@ -22,15 +22,16 @@ int main() {
         return 1;
     } else if (pid == 0) {
         // 자식 프로세스
-        printf("Child process: shared_data = %d (Address: %p)\n", *shared_data, shared_data);
+        printf("Child process: shared_data = %d\n", *shared_data);
         *shared_data = 84;  // 공유 데이터 변경
-        printf("Child process: shared_data changed to %d (Address: %p)\n", *shared_data, shared_data);
+        printf("Child process: shared_data changed to %d\n", *shared_data);
     } else {
         // 부모 프로세스
-        wait(NULL);  // 자식 프로세스가 종료될 때까지 대기
-        printf("Parent process: shared_data = %d (Address: %p)\n", *shared_data, shared_data);
+        wait(NULL);
+        printf("Parent process: shared_data = %d\n", *shared_data);
     }
 
+    // 메모리 매핑을 해제합니다.
     munmap(shared_data, size);
     return 0;
 }
